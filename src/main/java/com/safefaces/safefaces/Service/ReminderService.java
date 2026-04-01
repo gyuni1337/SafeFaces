83b8fa64b3834eef0244aaf14470ed78739da116
gyuni1337 <gyuni1337@gmail.com>
2026-04-01 20:34:58 +0200

    before making ReminderService

--- src/main/java/com/safefaces/safefaces/Service/ReminderService.java
+++ src/main/java/com/safefaces/safefaces/Service/ReminderService.java
@@ -1,4 +1,31 @@
 package com.safefaces.safefaces.Service;
 
 public class ReminderService {
+
+    // homeView: upcoming/reminders in time span. include at what time to take medications DAILY ( ONLY set by caregiver ) by default;
+    // profileView aka Jag view: caregiver at top with call now button. underneath is each medication information like dosage, time to take, etc.
+    // contactView: idk maybe see saved contacts and information about them - maybe personal note system
+
+    // required objects:
+    // Reminder
+    // Contact, must have creatorIDs ( for database specifically only 1 table to pull from )
+    // Caregiver extends User
+
+
+
+
+//    REMINDER object:
+
+//    message;
+//    description (optional );
+//    startTime;
+//    endTime;
+//    ownerId:
+
+//    REMINDER requiredFunctions:
+//    - createReminder(userId, to save to database as ownerId of reminder);
+//    - getUserReminders(userID == ownerId);
+
+
+
 }
