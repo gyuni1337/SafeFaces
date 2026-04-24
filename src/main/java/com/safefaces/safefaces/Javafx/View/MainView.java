507065065b5acab79abbac0f664950bddd984549
gyuni1337 <gyuni1337@gmail.com>
2026-04-24 12:06:51 +0200

    Switch to MainView.fxml, resize scene, add ContactView
    Update MainView to load the renamed FXML (MainView.fxml) instead of HomeView.fxml and increase the window height from 400 to 700. Add a new ContactView.fxml resource (AnchorPane + BorderPane layout, top brand, include bottom navigation, uses UserViewController). Also rename the existing HomeView.fxml to MainView.fxml to reflect the new entry view.

--- src/main/java/com/safefaces/safefaces/Javafx/View/MainView.java
+++ src/main/java/com/safefaces/safefaces/Javafx/View/MainView.java
@@ -20,7 +20,7 @@ public class MainView extends Application {
         @Override
         public void start(Stage stage) throws IOException {
 
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/safefaces/safefaces/HomeView.fxml"));
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/safefaces/safefaces/MainView.fxml"));
             Parent root = loader.load();
             UserViewController controller = loader.getController();
 //            controller.setUser(
@@ -32,7 +32,7 @@ public class MainView extends Application {
            // FXMLLoader fxmlLoader = new FXMLLoader(url);
 
 
-            Scene scene = new Scene(root, 400, 400);
+            Scene scene = new Scene(root, 400, 700);
             stage.setTitle("SafeFaces");
             stage.setScene(scene);
             stage.show();
